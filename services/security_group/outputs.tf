dac801211c0411c07d714eeec68186a74cb9fac8
aryanrhm <ary.rahimi@gmail.com>
2023-03-09 12:25:22 +0000

    chore(security_group): fix the output

--- services/security_group/outputs.tf
+++ services/security_group/outputs.tf
@@ -1,11 +1,11 @@
 output "id" {
-    value = aws_security_group.ec2_instance-sg.id
+    value = aws_security_group.security_group.id
     description = "The ID of created security group"
   
 }
 
 output "vpc_id" {
-    value = aws_security_group.ec2_instance-sg.vpc_id
+    value = aws_security_group.security_group.vpc_id
     description = "The VPC ID which the sg is created in"
   
 }
